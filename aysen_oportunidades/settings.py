222eda76c1ca3ebc6b6b33f540accc2c170cc6a0
alpharius4 <143748925+alpharius4@users.noreply.github.com>
2026-06-12 20:37:52 -0300

    fix: corregir secret_key y allowed_hosts

--- aysen_oportunidades/settings.py
+++ aysen_oportunidades/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tu-clave-secreta-aleatoria-aqui')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aysenoportunidades.fly.dev', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['aysenoportunidades.fly.dev', 'localhost', '127.0.0.1','*']
 
 CSRF_TRUSTED_ORIGINS = ['https://aysenoportunidades.fly.dev']
 
